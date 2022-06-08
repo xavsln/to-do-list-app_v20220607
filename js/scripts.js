fdb61390b5d54ef2af2b58d8c08b004aab5f30c1
xavsln <19362582+xavsln@users.noreply.github.com>
2022-06-08 09:12:55 +0700

    Use remove() instead of addClass('delete')

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,8 @@ function newItem(){
 
   // We create an eventlistener on the X button
   crossOutButton.on('click', ()=> {
-    li.addClass('delete');
+    // li.addClass('delete');
+    li.remove();
   });
 
   // We use the sortable() method from jQuery in order to be able to sort items inside our #list
